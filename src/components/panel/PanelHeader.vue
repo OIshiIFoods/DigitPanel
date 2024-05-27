be64dea3072768478c6d9ba2ad596de41de9d28d
LiZhen <3081085609@qq.com>
2024-05-27 14:10:35 +0800

    finish header

--- src/components/panel/PanelHeader.vue
+++ src/components/panel/PanelHeader.vue
@@ -1,7 +1,38 @@
 <template>
-  <header>数字面板头部</header>
+  <header class="header">
+    <h2 class="title">数字面板</h2>
+    <div class="time">
+      <span>当前时间:{{ curTime }}</span>
+    </div>
+  </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
 
-<style lang="stylus"></style>
+import { dateFormat } from '@/utils/DateFormat';
+let timeFormat = 'yyyy年MM月dd日-hh时mm分ss秒'
+let curTime = ref(dateFormat(timeFormat))
+setInterval(()=>{
+  curTime.value = dateFormat(timeFormat)
+},1000)
+
+</script>
+
+<style lang="stylus" scoped>
+.header
+  position relative
+  display flex
+  justify-content center
+  align-items center
+  background: url('@/assets/images/head_bg.png') center/cover no-repeat
+  & .title
+    color white
+  & .time
+    position absolute
+    right 30px
+    top calc(50% - 10px)
+    color rgba(255,255,255,.5)
+    line-height 20px
+    font-size 15px
+</style>
---
+++ src/utils/DateFormat.ts
@@ -0,0 +1,32 @@
+import { getVarType } from "./getVarType";
+
+/**
+ * @param date 
+ * @param format: '关键字: yyyy MM dd hh mm ss' 
+ */
+function dateFormat(format: string, date?: string | Date) {
+    let typeMap: { [field: string]: any } = {
+        'string': () => new Date(date || new Date()),
+        'Date': () => date
+    }
+    let dateObj: Date = typeMap[getVarType(date)]?.() ?? new Date()
+
+    let keywordsMap: { [field: string]: any } = {
+        'yyyy': dateObj.getFullYear(),
+        'MM': dateObj.getMonth() + 1,
+        'dd': dateObj.getDate(),
+        'hh': dateObj.getHours(),
+        'mm': dateObj.getMinutes(),
+        'ss': dateObj.getSeconds()
+    }
+
+    let dateStr = Object.entries(keywordsMap).reduce((acc, [key, value]) => {
+        return format.includes(key) ? acc.replace(key, value) : acc
+    }, format)
+
+    return dateStr
+}
+
+export {
+    dateFormat
+}
